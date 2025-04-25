f57c793f595e9d3f8bd7dfb868395230edd7f914
GlebHal <hammanov.gleb@gmail.com>
2025-04-25 14:17:59 +0300

    add exception handler

---
+++ patient-service/src/main/java/dev/haguel/patientservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package dev.haguel.patientservice.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = exception.getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        fieldError -> fieldError.getField(),
+                        fieldError -> fieldError.getDefaultMessage()
+                ));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
