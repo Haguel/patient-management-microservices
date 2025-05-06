775865df3bb1d4a09fabce39b83a5c13028fe482
GlebHal <hammanov.gleb@gmail.com>
2025-05-06 13:02:50 +0300

    setup auth service

---
+++ auth-service/src/main/java/dev/haguel/authservice/entity/User.java
@@ -0,0 +1,24 @@
+package dev.haguel.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private String role;
+}
